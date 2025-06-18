66b32a76e6e76e5c5387cd4b2dc4db92553bcbd8
Anurag Singh <anurag26official@gmail.com>
2025-06-18 21:24:29 +0530

    Fixing Bugs for deployment

--- layout.tsx
+++ layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/app/components/theme-provider';
-import { Toaster } from 'sooner';
 
 const inter = Inter({ subsets: ['latin'] });
 
