050fc0acecd633476bdc3c8909728958e41762cf
Anurag Singh <anurag26official@gmail.com>
2025-06-19 00:56:35 +0530

    FIxing deployment bugs

--- layout.tsx
+++ layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/app/components/theme-provider';
+import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'] });
 
